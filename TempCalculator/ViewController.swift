a58f2aacf7ed59e48106efb3139a7c723fd108fc
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-11-28 20:06:24 -0500

    made basic temp calculator

--- TempCalculator/ViewController.swift
+++ TempCalculator/ViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cTextField: UITextField!
+    @IBOutlet weak var kTextField: UITextField!
+    @IBOutlet weak var fTextField: UITextField!
+    
+    @IBOutlet weak var tempstringLabel: UILabel!
+    
+    @IBAction func convertTemp(_ sender: Any) {
+        
+        guard
+            let cTemp = Int(cTextField.text!)
+        else { return }
+        
+        //(0°C × 9/5) + 32 = 32°F
+        
+        fTextField.text = String((cTemp * 9 / 5) + 32)
+        kTextField.text = String(cTemp + 273)
+        
+        switch cTemp{
+        case ...0:
+            tempstringLabel.text = "its cold ouside"
+        case 1...30:
+            tempstringLabel.text = "its perfect outside"
+        case 31...:
+            tempstringLabel.text = "its boiling outside"
+        default:
+            tempstringLabel.text = "Tempeture is outside of normal range"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tempstringLabel.text = ""
     }
 
 
